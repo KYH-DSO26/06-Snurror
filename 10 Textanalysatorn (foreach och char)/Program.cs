7bcdc7364761ff890622cc3e5327fae6804f89df
Claes Engelin <claes.engelin@zocom.se>
2026-09-23 12:22:37 +0300

    Efter uppgift 10

---
+++ 10 Textanalysatorn (foreach och char)/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Skriv en mening: ");
+var mening = Console.ReadLine();
+int mellanslagRäknare = 0;
+
+// Snurra igenom hela strängen, ett tecken i taget
+foreach (var tecken in mening)
+{
+    if (tecken == ' ')
+    {
+        mellanslagRäknare++;
+    }
+}
+
+Console.WriteLine($"Meningen innehåller {mellanslagRäknare} stycken mellanslag");
+
+
+
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey(); Console.WriteLine("Hello, World!");
