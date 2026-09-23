9fc3db75a313c65de4fc60db532b8e30f74b54e8
Claes Engelin <claes.engelin@zocom.se>
2026-09-23 10:50:52 +0300

    Efter uppgift 4

---
+++ 04 Baklänges (for)/Program.cs
@@ -0,0 +1,15 @@
+﻿for (int i = 10; i >= 1; i--)
+{
+    Console.WriteLine(i);
+    Thread.Sleep(1000);      // Lite fördröjning för effekt
+}
+
+Console.WriteLine("\n\nGott nytt år!");
+
+
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
