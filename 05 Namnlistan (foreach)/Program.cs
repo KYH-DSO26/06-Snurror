8c8cdf100c25d024adecc2247a947dc522ae4185
Claes Engelin <claes.engelin@zocom.se>
2026-09-23 11:21:56 +0300

    Efter uppgift 6

---
+++ 05 Namnlistan (foreach)/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] namnLista = { "Anna", "Björn", "Cecilia", "Daniel", "Eva", "Filip" };
+
+foreach (var namn in namnLista)
+{
+    Console.WriteLine(namn.ToUpper());
+}
+
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
---
+++ 06 Sök efter guld (break)/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] ryggsäck = { "Rep", "Ficklampa", "Guld", "Karta", "Kompass" };
+
+foreach (var sak in ryggsäck)
+{
+    Console.WriteLine($"Tittar på: {sak}");
+    if (sak == "Guld")
+    {
+        Console.WriteLine("Hittade guld!");
+        break;      // Avbryter loopen omedelbart
+        //continue;     // Fortsätter med nästa varv i loopen
+    }
+}
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey(); Console.WriteLine("Hello, World!");
