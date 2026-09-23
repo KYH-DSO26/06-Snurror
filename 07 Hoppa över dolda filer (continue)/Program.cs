4455eb7051814fd5f9588e34cd374d7e2429df41
Claes Engelin <claes.engelin@zocom.se>
2026-09-23 11:38:04 +0300

    Efter uppgift 7

---
+++ 07 Hoppa över dolda filer (continue)/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] filer = { ".config", "index.html", ".git", "styles.css", "main.cs" };
+
+foreach (var fil in filer)
+{
+    if (fil.StartsWith("."))
+    {
+        continue;   // Hoppar direkt till nästa fil i loopen
+    }
+    Console.WriteLine($"Synlig fil: {fil}");
+}
+
+
+
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey(); Console.WriteLine("Hello, World!");
