91d0365afdb4a4711d6cd9de74dd7b7991296819
Claes Engelin <claes.engelin@zocom.se>
2026-09-23 11:47:49 +0300

    Efter uppgift 8

---
+++ 08 Multiplikationstabellen (Nästlade snurror)/Program.cs
@@ -0,0 +1,18 @@
+﻿for (int i = 1; i <= 5 ; i++)
+{
+    for (int j = 1; j <= 5; j++)
+    {
+        // Trimma och snygga till formateringen
+        Console.Write((i * j).ToString().PadLeft(6));
+        //Console.Write((i * j)+ "\t");
+    }
+    Console.WriteLine();    // Ny rad efter varje rad i tabellen
+}
+
+
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey(); Console.WriteLine("Hello, World!");
