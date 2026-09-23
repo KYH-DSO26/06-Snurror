fd6c9ae0528fe35a6d466b5a46ce25b6dd8a467f
Claes Engelin <claes.engelin@zocom.se>
2026-09-23 12:05:59 +0300

    Efter uppgift 9

---
+++ 09 Menyvalet med nödutgång (while-true och break)/Program.cs
@@ -0,0 +1,27 @@
+﻿while (true)
+{
+    Console.WriteLine("\n--- MENY ---");
+    Console.WriteLine("1. Visa profil");
+    Console.WriteLine("2. Ändra inställningar");
+    Console.WriteLine("3. Avsluta");
+    Console.Write("Ange ett alternativ: ");
+
+    var val = Console.ReadLine();
+    if (val == "1") Console.WriteLine("Visar profil...");
+    else if (val == "2") Console.WriteLine("Öppnar inställningar");
+    else if (val == "3")
+    {
+        Console.WriteLine("Aavslutar programmet...");
+        break;      // Bryter oss ur den oändliga loopen
+    }
+    else Console.WriteLine("Ogiltigt val, försök igen.");
+}
+
+
+
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey(); Console.WriteLine("Hello, World!");
