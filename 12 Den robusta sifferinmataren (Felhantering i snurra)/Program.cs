4e2c69147a5410916216ac8bb7989345fa923f95
Claes Engelin <claes.engelin@zocom.se>
2026-09-23 14:32:15 +0300

    Efter uppgift 12, sista för dagen

---
+++ 12 Den robusta sifferinmataren (Felhantering i snurra)/Program.cs
@@ -0,0 +1,34 @@
+﻿int giltigtTal = 0;
+//bool lyckades = false;
+bool lyckades;
+
+do
+//while (lyckades == false)
+//while (!lyckades)
+{
+    Console.Write("Ange ett heltal: ");
+    string inmatning = Console.ReadLine();
+
+    if (int.TryParse(inmatning, out giltigtTal))
+    {
+        lyckades = true;
+    }
+    else
+    {
+        Console.WriteLine("Felaktig inmatning. Det där var inte ett heltal!");
+        lyckades = false;
+    }
+}
+while (!lyckades);
+
+Console.WriteLine($"Tack! Du matade in talet: {giltigtTal}");
+
+
+
+
+
+
+#region Extrarader
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey(); Console.WriteLine("Hello, World!");
+#endregion
