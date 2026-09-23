f7e285377fe74055e531f4a8c7fb7cd119eb51da
Claes Engelin <claes.engelin@zocom.se>
2026-09-23 10:17:17 +0300

    Efter uppgift 2

--- 01 Välkomstsnurran (while)/Program.cs
+++ 01 Välkomstsnurran (while)/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string svar = "ja";
+while (svar.ToLower() == "ja" || svar.ToLower() == "j")
+{
+    Console.WriteLine("Snurrar vidare...");
+    Console.Write("Vill du fortsätta? (ja/nej): ");
+    svar = Console.ReadLine();
+    if (svar.ToLower() != "ja" && svar.ToLower() != "nej")
+    {
+        Console.WriteLine("Svara ja eller nej!");
+        svar = "ja";
+    }
+
+}
+
+Console.WriteLine("Programmet avslutat. Hejdå!");
+
+
+
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
---
+++ 02 Inmatningsskyddet (do-while)/Program.cs
@@ -0,0 +1,20 @@
+﻿string lösenord;
+do
+{
+    Console.Write("Ange lösenord: ");
+    lösenord = Console.ReadLine();
+    if (lösenord != "hemligt")
+    {
+        Console.WriteLine("Fel lösenord! Försök igen.");
+    }
+}
+while (lösenord != "hemligt");
+
+Console.WriteLine("Rätt! Du är inloggad.");
+
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
