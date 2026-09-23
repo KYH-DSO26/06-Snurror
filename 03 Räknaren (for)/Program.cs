21ad58cb9de19087949d7e2c09b853acb76d3e01
Claes Engelin <claes.engelin@zocom.se>
2026-09-23 10:42:06 +0300

    Efter uppgift 3

---
+++ 03 Räknaren (for)/Program.cs
@@ -0,0 +1,13 @@
+﻿int nVarv = 10;
+
+for (int i = 1; i <= nVarv; i++)
+{
+    Console.Write(i + "; ");
+}
+Console.WriteLine("\nFärdig!");
+
+
+
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
